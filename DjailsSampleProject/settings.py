68cbdbc3d69d56f31fb9fcb424d48a056c2195b0
Luis <luismasuelli@hotmail.com>
2013-09-18 00:40:15 -0300

    Creating and installing a sample application

--- DjailsSampleProject/settings.py
+++ DjailsSampleProject/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'djails',
+    'example'
 )
 
 # A sample logging configuration. The only tangible logging
