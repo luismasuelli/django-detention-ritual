6babad99f53e7e33a05df7bdbe59eca80853f6e4
Luis <luismasuelli@hotmail.com>
2013-10-07 23:17:23 -0300

    Controlling django version.

--- djails/__init__.py
+++ djails/__init__.py
@@ -1,1 +1,4 @@
-import configure
+from django import VERSION
+from django.core.exceptions import ImproperlyConfigured
+if VERSION[:2] < (1, 5):
+    raise ImproperlyConfigured("Django 1.5 is required for djails application to work")
